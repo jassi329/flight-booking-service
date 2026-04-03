5ab822e26b971f3a21d28cb9a336d6f1b7f8ed86
Jaskaran <jassisk329@gmailcom>
2026-04-03 12:48:22 +0530

    Refactor createBooking function to use async/await for improved error handling and integrate booking repository

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -7,19 +7,30 @@ const AppError = require('../utils/errors/app-error');
 const { StatusCodes } = require('http-status-codes');
 const { message } = require('../utils/common/error-response');
 
-async function createBooking(data) {
-    return new Promise((resolve, reject) => {
-        const result = db.sequelize.transaction(async function bookingImp(t) {
-            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
-            const flightData = flight.data.data;
-            if(data.noOfSeats > flightData.totalSeats) {
-                reject(new AppError('not enought seats available', StatusCodes.BAD_REQUEST));
-            }
-            resolve(true);
-        });
+const bookingRepository = new BookingRepository();
 
-        return result;
-    });
+async function createBooking(data) {
+    const transaction = await db.sequelize.transaction();
+    try{
+        const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data;
+        if(data.noOfSeats > flightData.totalSeats) {
+            throw new AppError('not enought seats available', StatusCodes.BAD_REQUEST);
+        }
+        const totalBillingAmount = data.noOfSeats * flightData.price; 
+        const bookingPayLoad = {...data, totalCost: totalBillingAmount};
+        const booking = await bookingRepository.createBooking(bookingPayLoad, transaction);
+
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}/seats`, {
+            seats: data.noOfSeats
+        })
+
+        await transaction.commit();
+        return(true);
+    }catch(error){
+        await transaction.rollback();
+        throw error;
+    }
 }
 
 module.exports = {
