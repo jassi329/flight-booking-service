4e0bf0c2b791f6c1457b55362e2d7438c5aa5c33
Jaskaran <jassisk329@gmailcom>
2026-07-18 13:26:28 +0530

    Add CORS middleware for frontend

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const CRON  = require('./utils/common/cron-jobs')
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
